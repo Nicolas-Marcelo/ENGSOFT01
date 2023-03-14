a7fc5e38d374f47600b1b2a5786c57e696608b63
Nicolas-Marcelo <86321896+Nicolas-Marcelo@users.noreply.github.com>
2023-03-14 11:13:30 -0300

    Create desafio-5.cpp

---
+++ desafio-5.cpp
@@ -0,0 +1,53 @@
+Codigo de numero 5
+
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+
+    int numero{10};    
+    //----ESCREVA SEU CÓDIGO ABAIXO DESTA LINHA----
+   
+    //-- Multiplica o número por 2 e atribui o resultado de volta ao número
+    double multiplica;
+    multiplica = numero*2;
+    cout << "mulitplica" << endl;
+
+
+    //-- Adiciona 9 ao número e atribui o resultado de volta ao número
+    double soma9;
+    soma9 = numero+9;
+    cout << soma9 << endl;
+
+
+    //-- Subtrair 3 do número e atribuir o resultado de volta ao número
+    double diminui3;
+    diminui3 = numero-3;
+    cout << diminui3 << endl;
+
+
+    //-- Divide o número por 2 e atribui o resultado de volta ao número
+    double divide2;
+    divide2 = numero/2;
+    cout << divide2 << endl;
+
+
+    //-- Subtrair original_number de number e atribuir o resultado de volta a number
+    double subtrai;
+    subtrai = numero-10;
+    cout << subtrai << endl;
+
+
+    //-- Pegue o módulo 3 (%) do número e atribua-o de volta ao número
+    double modulo;
+    modulo = numero%3;
+    outro = modulo+numero;
+    cout << outro << endl;
+   
+    //----ESCREVA SEU CÓDIGO ACIMA DESTA LINHA----
+   
+    return 0;
+}
